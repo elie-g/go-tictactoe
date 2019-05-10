4d913f84030e78266bbcc49c346e09f88f58468d
alex <alexanre.beaulieu94@gmail.com>
2019-05-10 10:50:38 -0400

    ajout validation sur grille courante

--- game/listeners.go
+++ game/listeners.go
@@ -1,34 +1,38 @@
 package game
 
 import (
-    . "github.com/DrunkenPoney/go-tictactoe/game/state"
-    . "github.com/DrunkenPoney/go-tictactoe/grid/tile"
+	. "github.com/DrunkenPoney/go-tictactoe/game/state"
+	. "github.com/DrunkenPoney/go-tictactoe/grid/tile"
 )
 
 // Private
 func (g *game) onClick() {
-    t, pos := g.board.GetTileUnderCursor()
-    if t != nil && t.Value == EMPTY {
-        if g.playerO.IsCurrent() {
-            t.Value = O
-        } else {
-            t.Value = X
-        }
-        g.GetBoard().DrawTile(t, pos)
-        g.NextTurn(t.Position)
-    }
+
+	t, pos := g.board.GetTileUnderCursor()
+	if t != nil {
+		if g.board.IsValidTile(t) && t.Value == EMPTY {
+			if g.playerO.IsCurrent() {
+				t.Value = O
+			} else {
+				t.Value = X
+			}
+			g.GetBoard().DrawTile(t, pos)
+			g.NextTurn(t.Position)
+		}
+	}
+
 }
 
 // Private
 func (g *game) onState(state State) {
-    if g.state != state {
-        switch state {
-        case PAUSED:
-            g.Pause()
-        case RUNNING:
-            g.Resume()
-        case STOPPED:
-            // TODO
-        }
-    }
+	if g.state != state {
+		switch state {
+		case PAUSED:
+			g.Pause()
+		case RUNNING:
+			g.Resume()
+		case STOPPED:
+			// TODO
+		}
+	}
 }
